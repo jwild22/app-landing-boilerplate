e5e34338cec910e207ce4f50ec51e6bd7a45b715
Farukh <lasamasa24@gmail.com>
2025-11-01 11:06:41 +0500

    update

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -123,6 +123,15 @@ export default async function LocaleLayout({
   return (
     <html lang={params.locale} suppressHydrationWarning>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZSW8HQN');`,
+          }}
+        />
         <meta name="apple-itunes-app" content="app-id=6743142831" />
         <script
           type="application/ld+json"
@@ -130,6 +139,14 @@ export default async function LocaleLayout({
         />
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZSW8HQN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
